f1616a159c69ee2c02c2abfd56ad48adfa70f3b5
Kris Bravo <kris.bravo@kachug.com>
2019-10-20 09:20:05 -0400

    Corrected org.

--- make-config.sh
+++ make-config.sh
@@ -21,6 +21,6 @@ cat ${BASE_CONFIG} \
     <(echo -e '</key>\n<tls-auth>') \
     ${KEY_DIR}/ta.key \
     <(echo -e '</tls-auth>') \
-    > ${OUTPUT_DIR}/Intermx\ Corporate\ VPN.ovpn
+    > ${OUTPUT_DIR}/Kachug\ Corporate\ VPN.ovpn
 
 # sed -i "s/group nogroup/group nobody/" ${OUTPUT_DIR}/${name}.ovpn
